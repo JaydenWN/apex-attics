817ddf65d3f9a3a9c9df9fea219b896a36da3105
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-08-12 15:38:42 +1000

    init final submission

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://apexattics.com",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		resolve: {
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Apex Attics - Central Coast Storage Conversion Experts';
+export const SITE_DESCRIPTION = 'Apex Attics are the #1 attic storage conversion experts for the central coast region and beyond.';
